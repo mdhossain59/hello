f4f9846f94211e0da2863f1a9b2ee0e29e7ac017
mdhossain59 <hossainmd754@gmail.com>
2022-02-07 09:26:57 -0500

    my first commit

---
+++ Method.java
@@ -0,0 +1,14 @@
+public class Method {
+
+	public static void main(String[] args) {
+		// for loop method.
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(" I am a An Automation Engineer");
+			System.out.println(" I am becoming Automation Engineer very soon.");
+			
+		}
+		
+	}
+	
+	}
